a73680ae24135a55df7fde0879e651427111e493
Ata KALDIRAK <atakldrkpersonal@gmail.com>
2025-01-23 03:06:32 +0300

    connect disconnect butonları eklendi ve işlevselleştirildi

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lblComPort = new System.Windows.Forms.Label();
-            this.cbComPort = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.btnConnect = new System.Windows.Forms.Button();
             this.btnDisconnect = new System.Windows.Forms.Button();
+            this.cbBaudrate = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbComPort = new System.Windows.Forms.ComboBox();
+            this.lblComPort = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.btnDisconnect);
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.cbBaudrate);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.cbComPort);
             this.groupBox1.Controls.Add(this.lblComPort);
@@ -52,15 +54,33 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Device Connection";
             // 
-            // lblComPort
+            // btnDisconnect
             // 
-            this.lblComPort.AutoSize = true;
-            this.lblComPort.Location = new System.Drawing.Point(6, 25);
-            this.lblComPort.Name = "lblComPort";
-            this.lblComPort.Size = new System.Drawing.Size(56, 13);
-            this.lblComPort.TabIndex = 0;
-            this.lblComPort.Text = "COM Port:";
-            this.lblComPort.Click += new System.EventHandler(this.lblComPort_Click);
+            this.btnDisconnect.Location = new System.Drawing.Point(96, 96);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 2;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // cbBaudrate
+            // 
+            this.cbBaudrate.FormattingEnabled = true;
+            this.cbBaudrate.Location = new System.Drawing.Point(68, 54);
+            this.cbBaudrate.Name = "cbBaudrate";
+            this.cbBaudrate.Size = new System.Drawing.Size(103, 21);
+            this.cbBaudrate.TabIndex = 3;
+            this.cbBaudrate.Text = "9600";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Baud Rate: ";
             // 
             // cbComPort
             // 
@@ -72,23 +92,15 @@
             this.cbComPort.Text = "COM13";
             this.cbComPort.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
-            // label1
+            // lblComPort
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 57);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Baud Rate: ";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(68, 54);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(103, 21);
-            this.comboBox1.TabIndex = 3;
-            this.comboBox1.Text = "9600";
+            this.lblComPort.AutoSize = true;
+            this.lblComPort.Location = new System.Drawing.Point(6, 25);
+            this.lblComPort.Name = "lblComPort";
+            this.lblComPort.Size = new System.Drawing.Size(56, 13);
+            this.lblComPort.TabIndex = 0;
+            this.lblComPort.Text = "COM Port:";
+            this.lblComPort.Click += new System.EventHandler(this.lblComPort_Click);
             // 
             // btnConnect
             // 
@@ -100,14 +112,9 @@
             this.btnConnect.UseVisualStyleBackColor = true;
             this.btnConnect.Click += new System.EventHandler(this.button1_Click);
             // 
-            // btnDisconnect
+            // serialPort1
             // 
-            this.btnDisconnect.Location = new System.Drawing.Point(96, 96);
-            this.btnDisconnect.Name = "btnDisconnect";
-            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
-            this.btnDisconnect.TabIndex = 2;
-            this.btnDisconnect.Text = "Disconnect";
-            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.serialPort1.PortName = "COM13";
             // 
             // Form1
             // 
@@ -117,7 +124,8 @@
             this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
-            this.Text = "Zagora Space Yer İstasyonu";
+            this.Text = " Zaground V1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -130,9 +138,10 @@
         private System.Windows.Forms.Label lblComPort;
         private System.Windows.Forms.ComboBox cbComPort;
         private System.Windows.Forms.Button btnDisconnect;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbBaudrate;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnConnect;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,7 +30,43 @@ namespace yeristasyonu
 
         private void button1_Click(object sender, EventArgs e)
         {
+            serialPort1.PortName = cbComPort.Text; // Düzeltildi
+            serialPort1.BaudRate = 9600;
 
+            try
+            {
+                serialPort1.Open();
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+                cbComPort.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                btnConnect.Enabled = true;
+                btnDisconnect.Enabled = false;
+            } // Kapanış düzeltildi
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String[] ports = SerialPort.GetPortNames();
+            cbComPort.Items.AddRange(ports);
+
+            btnConnect.Enabled = true;
+            btnDisconnect.Enabled = false;
+            cbComPort.Enabled = true;
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                btnConnect.Enabled = true;
+                btnDisconnect.Enabled = false;
+                cbComPort.Enabled = true;
+            }
         }
     }
 }
